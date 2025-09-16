a7f6498ae242345e3622ad343d3c79060652578b
Elisa Byfield <elisabyfield@gmail.com>
2025-09-16 14:13:06 -0700

    breakout room problems

--- codepath_1.py
+++ codepath_1.py
@@ -1,1 +1,12 @@
-print("Hello")
+'''
+# problem 1 
+def welcome():
+    print("Welcome to The Hundred Acre Wood")
+welcome()
+
+# problem 2 
+def greeting(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.") # using printf 
+greeting("Elisa")
+'''
+
